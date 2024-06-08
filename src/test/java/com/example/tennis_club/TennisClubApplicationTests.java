b2f77f0dab8f4318027a2f52a2cc1f37b08228dc
Peter Drazkovec <xdrazkov@fi.muni.cz>
2024-06-08 22:33:31 +0200

    init

---
+++ src/test/java/com/example/tennis_club/TennisClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tennis_club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
