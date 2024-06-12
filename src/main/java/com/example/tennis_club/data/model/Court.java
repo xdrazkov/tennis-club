f242c4dd6c99d981a507b758ca6316734095457f
Peter Drazkovec <xdrazkov@fi.muni.cz>
2024-06-12 13:00:06 +0200

    Add Court

--- src/main/java/com/example/tennis_club/data/model/Court.java
+++ src/main/java/com/example/tennis_club/data/model/Court.java
@@ -1,36 +1,32 @@
 package com.example.tennis_club.data.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @Entity
-@Table(name = "surface_type")
+@Table(name = "court")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
-public class SurfaceType {
+public class Court {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "surface_type_id")
+    @Column(name = "court_id")
     private Long id;
 
     @Column
     private String name;
 
-    @Column
-    private int pricePerMinute;
-
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "surfaceType", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE})
-    private List<Court> courts;
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "surface_type_id")
+    private SurfaceType surfaceType;
 }
