d822d5a797a5c88b117ac09875794ffb6e40b4b6
Peter Drazkovec <xdrazkov@fi.muni.cz>
2024-06-12 13:02:15 +0200

    Add Customer

---
+++ src/main/java/com/example/tennis_club/data/model/Customer.java
@@ -0,0 +1,25 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String phoneNumber;
+}
