cfbf335dc6a4191a6a01f071a882a1d30b36c95e
Peter Drazkovec <xdrazkov@fi.muni.cz>
2024-06-15 11:29:40 +0200

    Change dateFrom and DateTo to valid example

--- src/main/java/com/example/tennis_club/api/ReservationCreateDto.java
+++ src/main/java/com/example/tennis_club/api/ReservationCreateDto.java
@@ -7,16 +7,16 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Getter @Setter
-@Schema(name = "ReservationCreateDTO", description = "Represents a reservation")
+@Schema(name = "ReservationCreateDto", description = "Represents a reservation")
 public class ReservationCreateDto {
 
     @Schema(description = "Court ID", example = "1")
     private Long courtId;
 
-    @Schema(description = "Date and time of start of reservation", example = "2021-12-01 12:00:00")
+    @Schema(description = "Date and time of start of reservation", example = "2024-06-15T10:00:00.000Z")
     private LocalDateTime dateFrom;
 
-    @Schema(description = "Date and time of end of reservation", example = "2021-12-01 13:00:00")
+    @Schema(description = "Date and time of end of reservation", example = "2024-06-15T12:00:00.000Z")
     private LocalDateTime dateTo;
 
     @Schema(description = "Is doubles game", example = "true")
